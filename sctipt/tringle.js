a61619d1650d3f9e881bf33545edda86f02fb771
GolamRabbani <mdgolamrabbani738@gmail.com>
2024-02-13 09:42:13 +0600

    first commit

---
+++ sctipt/tringle.js
@@ -0,0 +1,26 @@
+
+// Tringle claculet
+function tringleCalculate() {
+    // baseCalculet
+    const baseValue = document.getElementById('base').value;
+    //convart baseValue from string to number
+    const base = parseFloat(baseValue);
+
+    // heightCalculet
+    const heightValue = document.getElementById('height').value;
+    //convart heightValue from string to number
+    const height = parseFloat(heightValue);
+
+
+    // area claculet
+    const area = 0.5 * base * height;
+
+    // area transform disply
+    const tringleAreaResult = document.getElementById('Area');
+    tringleAreaResult.innerText = area;
+
+
+
+
+}
+
